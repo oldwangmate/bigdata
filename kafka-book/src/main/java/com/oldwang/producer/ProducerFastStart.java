b6c1c7a884f0fcfcdbcef6ad2bbb4a82ee3f3013
wang.heng <wang.heng@navercorp.com>
2020-12-12 21:01:20 +0800

    代码封装，优化

--- kafka-book/src/main/java/com/oldwang/producer/ProducerFastStart.java
+++ kafka-book/src/main/java/com/oldwang/producer/ProducerFastStart.java
@@ -2,6 +2,7 @@ package com.oldwang.producer;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
 
@@ -14,12 +15,14 @@ public class ProducerFastStart {
     private static final String TOPIC = "kafka-demo";
 
     public static void main(String[] args) {
-        Properties properties = new Properties();
-        properties.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
-        properties.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
-        properties.put("bootstrap.servers",BROKER_LIST);
-        //配置生产者客户端参数并创建kafkaProducer实例
-        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+//        Properties properties = new Properties();
+//        //序列化方式 全限定类名
+//        properties.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
+//        //可通过类名获取 提高代码简洁度
+//        properties.put("value.serializer", StringSerializer.class.getName());
+//        properties.put("bootstrap.servers",BROKER_LIST);
+//        //配置生产者客户端参数并创建kafkaProducer实例
+        KafkaProducer<String, String> producer = new KafkaProducer<>(initConfig());
         //构建所需要发送的消息
         ProducerRecord<String,String> record = new ProducerRecord<>(TOPIC,"hello kafka");
         //发送消息
@@ -27,5 +30,16 @@ public class ProducerFastStart {
         //关闭生产者客户端
         producer.close();
 
+    }
+
+    //封装配置文件
+    private static Properties initConfig(){
+        Properties properties = new Properties();
+        //序列化方式 全限定类名
+        properties.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
+        //可通过类名获取 提高代码简洁度
+        properties.put("value.serializer", StringSerializer.class.getName());
+        properties.put("bootstrap.servers",BROKER_LIST);
+        return properties;
     }
 }
